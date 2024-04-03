2be9d0cc2aa8801f2c10c0120020a5399b31cc34
Antonin Blot <antonin.blot@crick.ac.uk>
2024-04-03 11:46:28 +0100

    [feature] add "find_affine_by_block"
    Run the correlation by block then fit affine transform with huber regressor

--- image_tools/registration/affine_by_block.py
+++ image_tools/registration/affine_by_block.py
@@ -2,8 +2,81 @@ from typing import Optional
 
 import numpy as np
 import numpy.typing as npt
+from sklearn.linear_model import HuberRegressor
 
 from . import phase_correlation as _pc
+
+
+def find_affine_by_block(
+    reference: npt.NDArray,
+    target: npt.NDArray,
+    block_size: int = 256,
+    overlap: float = 0.5,
+    correlation_threshold: Optional[float] = None,
+):
+    """Find affine transformation between two images by dividing them into blocks and
+    estimating translation for each block.
+
+    We fit:
+    x_coords = a_x * x + b_x * y + c_x
+    y_coords = a_y * x + b_y * y + c_y
+
+    Args:
+        reference (np.array): reference image
+        target (np.array): target image
+        block_size (int, optional): size of the blocks, defaults to 256
+        overlap (float, optional): fraction of overlap between blocks, defaults to 0.5
+        correlation_threshold (float, optional): minimum correlation threshold, defaults
+            to None
+
+    Returns:
+        affine_transform (function): function that gives the affine transformation for
+            any point
+        x_params (np.array): parameters (a_x, b_x, c_x) of the affine transformation in
+            x direction
+        y_params (np.array): parameters (a_y, b_y, c_y) of the affine transformation in
+            y direction
+    """
+
+    # first perform phase correlation by block
+    shifts, corr, centers = phase_correlation_by_block(
+        reference, target, block_size=128, overlap=0.7
+    )
+    # then fit affine transformation to the shifts
+    shifts = shifts.reshape(-1, 2)
+    hom_centers = centers.reshape(-1, 2)
+
+    if correlation_threshold is not None:
+        corr = corr.reshape(-1)
+        shifts = shifts[corr > correlation_threshold]
+        hom_centers = hom_centers[corr > correlation_threshold]
+
+    # minor annoyance, if shifts are all exactly the same, the HuberRegressor will
+    # sometimes fail to converge, in that case we add a small amount of noise
+    if np.sum(shifts - shifts[0]) < 1:
+        shifts += np.random.normal(0, 0.01, shifts.shape)
+
+    huber_x = HuberRegressor(fit_intercept=True).fit(hom_centers, shifts[:, 0])
+    huber_y = HuberRegressor(fit_intercept=True).fit(hom_centers, shifts[:, 1])
+
+    # finally make a function that gives the affine transformation for any point
+    def affine_transform(point):
+        if not isinstance(point, np.ndarray):
+            point = np.array(point)
+        if point.ndim == 1:
+            point = point.reshape(1, -1)
+        shift = np.hstack(
+            [
+                huber_x.predict(point).reshape(1, -1),
+                huber_y.predict(point).reshape(1, -1),
+            ]
+        )
+        new_point = point + shift
+        return new_point
+
+    x_params = np.hstack([huber_x.coef_, huber_x.intercept_])
+    y_params = np.hstack([huber_y.coef_, huber_y.intercept_])
+    return affine_transform, x_params, y_params
 
 
 def phase_correlation_by_block(
@@ -12,7 +85,7 @@ def phase_correlation_by_block(
     block_size: int = 256,
     overlap: float = 0.1,
 ):
-    """Estimate translation between two images by dividing them into blocks and 
+    """Estimate translation between two images by dividing them into blocks and
     estimating translation for each block.
 
     Args:
--- tests/test_unit/test_affine_by_block.py
+++ tests/test_unit/test_affine_by_block.py
@@ -6,13 +6,52 @@ from image_tools.registration import affine_by_block as abb
 
 def test_phase_corr_by_block():
     true_shifts = (10, 15)
-    
+
     # Make test data
     cat = data.cat()
     fixed_image = cat[:256, :256, 0]
-    moving_image = cat[true_shifts[0]:256 + true_shifts[0], true_shifts[1]:256 + true_shifts[1], 0]
-    shifts, corrs, centers = abb.phase_correlation_by_block(fixed_image, moving_image, block_size=156, overlap=0.8)
+    moving_image = cat[
+        true_shifts[0] : 256 + true_shifts[0], true_shifts[1] : 256 + true_shifts[1], 0
+    ]
+
+    shifts, corrs, centers = abb.phase_correlation_by_block(
+        fixed_image, moving_image, block_size=156, overlap=0.8
+    )
     assert np.mean(corrs) > 0.5
+    assert corrs.shape == shifts.shape[:2]
     assert centers.shape == shifts.shape
     shift = np.nanmedian(shifts, axis=(0, 1))
     assert np.allclose(shift, true_shifts, atol=1)
+
+
+def test_affine_by_block():
+    true_shifts = (10, 15)
+    # Make test data
+    cat = data.cat()
+    fixed_image = cat[:256, :256, 0]
+    moving_image = cat[
+        true_shifts[0] : 256 + true_shifts[0], true_shifts[1] : 256 + true_shifts[1], 0
+    ]
+
+    affin, param_x, param_y = abb.find_affine_by_block(
+        fixed_image,
+        moving_image,
+        block_size=156,
+        overlap=0.8,
+        correlation_threshold=0.4,
+    )
+    # a_x, a_y, b_x, and b_y should be very small
+    assert np.all(np.abs(param_x[:2]) < 0.01)
+    assert np.all(np.abs(param_y[:2]) < 0.01)
+    # c_x and c_y should be close to the true shifts
+    assert np.allclose(param_x[2], true_shifts[0], atol=1)
+    assert np.allclose(param_y[2], true_shifts[1], atol=1)
+    # Test the affine transformation
+    point = np.array([10, 10])
+    assert np.allclose(affin(point)[0], point + true_shifts, atol=1)
+
+
+if __name__ == "__main__":
+    test_affine_by_block()
+    test_phase_corr_by_block()
+    print("Everything passed")
